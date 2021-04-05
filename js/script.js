830becbd9da8930662a0de618467defb3d4cbe82
Stanislav Tabachnikov <78046352+tabakus@users.noreply.github.com>
2021-04-05 19:50:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    //page up:
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    //плавный скролл:
+
+    $("a[href='#up']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+});
