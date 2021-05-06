36473724907238b1cd24cc485c3dd0600bdd0251
Stanislav Tabachnikov <78046352+tabakus@users.noreply.github.com>
2021-05-07 01:33:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //page up:
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    //плавный скролл:
+
+    $("a[href='#up']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+});
+
+const hamburger = document.querySelector('.promo__burger'),
+      menu = document.querySelector('.promo__menu');
+      
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('promo__burger-active');
+    menu.classList.toggle('promo__menu-active');
+});
